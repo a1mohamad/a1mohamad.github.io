e484726b15d159f8be378fc7b392916a0c247d8a
Amir Mohamad Askari <a1.mohamad.askari@gmail.com>
2026-05-29 10:01:08 +0330

    Create script.js

---
+++ research/heart-disease/script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const blocks = document.querySelectorAll('.metric-block');
+    blocks.forEach(block => {
+        const targetEl = block.querySelector('.metric-value');
+        const type = block.getAttribute('data-metric-type');
+        const targetVal = parseFloat(block.getAttribute('data-metric-target'));
+        
+        let initial = 0;
+        const runtime = 1200;
+        const steps = 40;
+        const intervalVal = targetVal / steps;
+        let currentStep = 0;
+
+        const increment = () => {
+            currentStep++;
+            initial += intervalVal;
+            
+            if (currentStep < steps) {
+                targetEl.innerText = type === 'percentage' ? initial.toFixed(2) + '%' : Math.floor(initial);
+                setTimeout(increment, runtime / steps);
+            } else {
+                targetEl.innerText = type === 'percentage' ? targetVal.toFixed(2) + '%' : targetVal;
+            }
+        };
+        increment();
+    });
+});
